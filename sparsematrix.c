e4d0f28dc8fcb24a637d87a7e5b56ff428781ec5
Kushagra Srivastava <infinityblaze333@gmail.com>
2023-11-07 04:58:17 +0530

    no fastforward now

---
+++ sparsematrix.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int sparseMatrix[5][5] = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 8, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 5}
+    };
+
+    int rows = 5;
+    int cols = 5;
+
+    int nonZeroCount = 0;
+
+    // Count the number of non-zero elements in the sparse matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (sparseMatrix[i][j] != 0) {
+                nonZeroCount++;
+            }
+        }
+    }
+
+    // Create arrays to store the non-zero values and their corresponding row and column indices
+    int nonZeroValues[nonZeroCount];
+    int rowIndices[nonZeroCount];
+    int colIndices[nonZeroCount];
+
+    int index = 0;
+
+    // Populate the arrays with non-zero values and their indices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (sparseMatrix[i][j] != 0) {
+                nonZeroValues[index] = sparseMatrix[i][j];
+                rowIndices[index] = i;
+                colIndices[index] = j;
+                index++;
+            }
+        }
+    }
+
+    // Print the sparse matrix
+    printf("Sparse Matrix:\n");
+    for (int i = 0; i < nonZeroCount; i++) {
+        printf("%d at (%d, %d)\n", nonZeroValues[i], rowIndices[i], colIndices[i]);
+    }
+
+    return 0;
+}
